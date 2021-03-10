926c8eb0700476311f92408856c5f7ff4bd44c7b
Himanshu <himanshu.thakur@mobicules.com>
2021-03-10 17:10:20 +0530

    api integrated at detail n listing page

--- HirePress/HirePress/Scripts/model/candidate-index.js
+++ HirePress/HirePress/Scripts/model/candidate-index.js
@@ -1,13 +1,48 @@
-﻿$.ajax({
+﻿
+$.ajax({
     url: "/api/alljob",
     type: "GET",
     success: function (data) {
         console.log(data);
+        generateJobs(data)
     },
     error: function (err) {
         alert(err.responseText);
     }
 });
+
+function generateJobs(data) {
+    data.forEach(e => {
+        const payload = ` <div class="job-listings">
+                        <div class="row">
+                            <div class="col-lg-1 col-md-1 col-xs-12">
+                                <div class="job-company-logo">
+                                    <img src="~/Theme/homepage-assets/assets/img/features/img1.png" alt="">
+                                </div>
+
+                            </div>
+                            <div class="col-lg-9 col-md-9 col-xs-12 text-left pl-4">
+                                <div class="job-details">
+                                    <h3>${e.JobTitle}</h3>
+                                    <div class="location mt-0">
+                                        <i class="lni-map-marker"></i> ${e.Location}
+                                    </div>
+                                </div>
+                                <p>
+
+                                    ${e.Description.slice(0,200)}
+
+                                </p>
+                            </div>
+                            <div class="col-lg-2 col-md-2 col-xs-12 text-center job-posting-action">
+                                <a href="${e.JobURL} class="btn-apply"><span class="btn-apply">View</span> </a><br>
+                                <span class="btn-full-time">Not intrested</span>
+                            </div>
+                        </div>
+                    </div>`
+        $('.jobs-wrapper').append(payload)
+    })
+}
 
 $(document).ready(() => { 
     CreateCityDropdown();
