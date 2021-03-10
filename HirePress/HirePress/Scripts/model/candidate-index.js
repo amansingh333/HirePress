cf8cdf93c9ddb5ba6632fb0c57c771f0789c0476
Arjit Singh <73281527+ArjitSingh28@users.noreply.github.com>
2021-03-10 15:08:46 +0530

    commit

--- HirePress/HirePress/Scripts/model/candidate-index.js
+++ HirePress/HirePress/Scripts/model/candidate-index.js
@@ -8,3 +8,23 @@
         alert(err.responseText);
     }
 });
+
+$(document).ready(() => { 
+    CreateCityDropdown();
+});
+
+function CreateCityDropdown() {
+    
+    $.getJSON('Json/cities.json', function (data) {
+        $('#city_dropdown').html('');
+        $('#city_dropdown').append('<input type="text" class="form-control" id="txt-search" name="SearchCity" list="city" placeholder="Location: City, State, Zip" autocomplete="off">');
+        var datalist = '<datalist id="city">';
+        $.each(data, function (key, val) {
+            datalist += '<option value="' + val.name + '">';
+        });
+        datalist += '</datalist>';
+        $('#city_dropdown').append(datalist);
+    }).fail(function (err) { alert(err.responseText); });
+    
+}
+
